9f590ce6edc7ce919e9e67c7dce079cf698458d7
Non <Kitanon_san@cmu.ac.th>
2021-12-17 16:45:16 +0700

    Lab11_3

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,25 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main(){
+    ifstream score("score.txt");
+    double sum = 0,xsquare = 0,Mean,SD;
+    int N = 0;
+    string text;
+    while(getline(score,text)){
+        sum += atof(text.c_str());
+        xsquare += pow(atof(text.c_str()),2);
+        N++;
+    }
+    Mean = sum/N;
+    SD = sqrt((xsquare/N)-pow(Mean,2));
+
+    cout << "Number of data = " << N << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << Mean << "\n";
+    cout << "Standard deviation = " << SD << "\n";
+}
